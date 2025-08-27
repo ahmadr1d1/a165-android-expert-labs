7fe052dc9935401bd6d7b689007d35368228fc6c
Ahmad RD <ahmaddahuri212@gmail.com>
2025-08-27 16:56:22 +0700

    Migrate from Groovy to Gradle Kotlin DSL and update dependencies
    This commit migrates the MySimpleLogin (starter) project from Groovy-based Gradle scripts to Kotlin DSL.
    
    Key changes include:
    - Renamed `build.gradle` and `settings.gradle` to `build.gradle.kts` and `settings.gradle.kts` respectively.
    - Updated Gradle wrapper to version 8.14.3
    - Implemented version catalog (`libs.versions.toml`) for managing dependencies.
    - Updated various dependencies to their latest versions, including:
        - Android Gradle Plugin to 8.11.1
        - Kotlin to 2.2.10
        - Appcompat to 1.7.1
        - Core KTX to 1.17.0
        - Material Design to 1.12.0
        - ConstraintLayout to 2.2.1
        - JUnit to 4.13.2
        - AndroidX JUnit to 1.3.0
        - Espresso to 3.7.0
    - Updated compile SDK to 36 and target SDK to 36.
    - Set Java compatibility to version 17.
    - Enabled edge-to-edge display in `MainActivity`.
    - Changed app theme to `Theme.Material3.Light.NoActionBar` and `Theme.Material3.Dark.NoActionBar`.
    - Removed `package` attribute from `AndroidManifest.xml` as it's now inferred from `namespace` in `build.gradle.kts`.
    - Added new `.idea` configuration files.

--- MySimpleLogin/MySimpleLogin (starter)/app/src/main/java/com/dicoding/mysimplelogin/MainActivity.kt
+++ MySimpleLogin/MySimpleLogin (starter)/app/src/main/java/com/dicoding/mysimplelogin/MainActivity.kt
@@ -3,6 +3,9 @@ package com.dicoding.mysimplelogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.dicoding.mysimplelogin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +15,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         val sesi = SessionManager(this)
         userRepository = UserRepository.getInstance(sesi)
